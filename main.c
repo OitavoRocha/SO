dd6d6ef290505f565dcc7b0089c88e5b1b7568b0
Otávio Rocha <otaviosrocha30@gmail.com>
2024-02-28 20:15:16 -0300

    Updated main.c

--- main.c
+++ main.c
@@ -4,30 +4,32 @@
 #include<stdlib.h>
 #include<semaphore.h>
 
-#define N 6 // numero de cadeiras de espera
+#define N 3 // numero de cadeiras de espera
 #define M 4 // numero de barbeiros
 
 int clientesAtendidos = 0;
 int clientesDesistiram = 0;
 int cadeirasDisponiveis = N;
 int MAX_CLIENTES;
+int numClientes = 0;
 
 sem_t mutex;
 sem_t barbeiros;
 sem_t clientes;
-sem_t pentes;
-sem_t tesouras;
+//sem_t pentes;
+//sem_t tesouras;
 
 void * barbeiro ( void *arg );
 void * cliente ( void *arg );
 
 int main () {
     printf("Iniciando barbearia\n");
-    printf("Informe a quantidade máxima de clientes: ");
+    printf("Informe a quantidade maxima de clientes: ");
     scanf("%d", &MAX_CLIENTES);
 
 
     int idsClientes[MAX_CLIENTES];
+    int idBarbeiros[M];
     pthread_t tBarbeiros[M];
     pthread_t tClientes[MAX_CLIENTES];
 
@@ -38,17 +40,19 @@ int main () {
     sem_init( &clientes, 0, 0 );
 
     for ( int  i = 0 ; i < M ; i++ ) {
-        pthread_create( &tBarbeiros[i], NULL, barbeiro, NULL );
+        idBarbeiros[i] = i + 1;
+        pthread_create( &tBarbeiros[i], NULL, barbeiro, &idBarbeiros[i] );
     }
 
     for ( int i = 0 ; i < MAX_CLIENTES ; i++ ) {
         idsClientes[i] = i + 1;
         pthread_create( &tClientes[i], NULL, cliente, &idsClientes[i] );
-        sleep(rand() % 4 + 1); // tempo de chegada do cliente
+        sleep(rand() % 2 + 1); // tempo de chegada do cliente
     }
 
     for ( int i = 0 ; i < N + M ; i++ ) {
         pthread_join( tClientes[i], NULL );
+        //pthread_join( tBarbeiros[i], NULL );
     }
 
     printf("Clientes atendidos: %d\n", clientesAtendidos);
@@ -61,15 +65,19 @@ void * barbeiro( void *arg ) {
 
     while( 1 ) {
     sem_wait( &mutex );
-        if ( cadeirasDisponiveis == N && MAX_CLIENTES == clientesAtendidos + clientesDesistiram ) {
+        if ( cadeirasDisponiveis == N && numClientes == MAX_CLIENTES ) {
+            printf("Barbeiro %d encerrando expediente\n", *((int *) arg));
             sem_post( &mutex );
             pthread_exit(0);
         }
+        sem_post( &mutex );
+        printf("Barbeiro %d esperando Cliente\n", *((int *) arg));
         sem_wait( &clientes );
+        printf("Barbeiro %d esperando por equipamento\n", *((int *) arg));
         sem_wait( &mutex );
         cadeirasDisponiveis++;
-        sem_post( &barbeiros );
         sem_post( &mutex );
+        sem_post( &barbeiros );
     }
     
     return NULL;
@@ -79,27 +87,30 @@ void * cliente( void *arg ) {
     int idCliente = *((int *) arg);
     int tempoCorte = rand() % 4 + 3;
 
+    
+    
+    printf("Cliente %d chegou e esta esperando\n", idCliente);
     sem_wait( &mutex );
-    if ( MAX_CLIENTES == clientesAtendidos + clientesDesistiram ) {
+    numClientes++;
+
+    if ( cadeirasDisponiveis > 0 ) {
+        cadeirasDisponiveis--;
+        printf("Cadeiras disponiveis: %d\n", cadeirasDisponiveis);
+        sem_post( &clientes );
         sem_post( &mutex );
-        pthread_exit(0);
-    } else {
-        if ( cadeirasDisponiveis > 0 ) {
-            cadeirasDisponiveis--;
-            sem_post( &clientes );
-            sem_post( &mutex );
-            sem_wait( &barbeiros );
-            printf("Cliente %d cortando cabelo\n", idCliente);
-            sleep(tempoCorte);
-            sem_wait( &mutex );
-            clientesAtendidos++;
-            sem_post( &mutex );
-        } else {
-            printf("Cliente %d desistiu\n", idCliente);
-            clientesDesistiram++;
-            sem_post( &mutex );
-        }
+        sem_wait( &barbeiros );
+        printf("Cliente %d cortando cabelo\n", idCliente);
+        sleep(tempoCorte);
+        sem_wait( &mutex );
+        clientesAtendidos++;
+        printf("Cliente %d cortou o cabelo\n", idCliente);
+        sem_post( &mutex );
+    } else  {
+        printf("Cliente %d desistiu\n", idCliente);
+        clientesDesistiram++;
+        sem_post( &mutex );
     }
+    
 
     return NULL;
 }
